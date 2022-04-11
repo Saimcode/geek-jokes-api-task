f6cae2d3c3c477fdcaa34b0ad8e072ae3c697b38
Saim <86623899+Saimcode@users.noreply.github.com>
2022-04-11 08:44:13 +0100

    Update index.js

--- Jokes Generator/js/index.js
+++ Jokes Generator/js/index.js
@@ -1,8 +1,11 @@
+// Creating variables
 const button = document.querySelector(".container button");
 const jokeDiv = document.querySelector(".container .joke p");
 
+// When the website refreshes, a new joke is shown. (Instead of having black container or dummy text)
 document.addEventListener("DOMContentLoaded", getJoke);
 
+// Get a new joke when the button is clicked
 button.addEventListener("click", getJoke);
 
 async function getJoke() {
@@ -12,8 +15,8 @@ async function getJoke() {
             Accept: "application/json"
         }
     });
-    // Presenting on HTML file
+    // Converting the Fetched Data into .json file
     const jokeObj = await jokeData.json();
+    // Presenting only the joke on HTML file
     jokeDiv.innerHTML = jokeObj.joke;
-    console.log(jokeData);
 }
